592dfa398c06bcc58142c41f909278c79e10e604
Rupu21 <boserupam21@gmail.com>
2020-03-14 02:08:00 +0530

    retrive on search bar

--- app/src/main/java/com/example/dictionary/MainActivity.java
+++ app/src/main/java/com/example/dictionary/MainActivity.java
@@ -8,28 +8,51 @@ import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
+import android.widget.Toast;
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
     ArrayAdapter<String> arrayAdapter;
+    Trie trie=new Trie();
+    void load(){
+        BufferedReader reader ;
+        try{
+            reader = new BufferedReader(
+                    new InputStreamReader(getAssets().open("positive-words.txt")));
+            int flag=0,counter=0;
+            String mWord;
+            while ((mWord = reader.readLine()) != null) {
+                trie.insert(mWord);
+                mWord="";
+            }
+            //Toast.makeText(this,mLine,Toast.LENGTH_SHORT).show();
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+        }catch (IOException e) {
+            Toast.makeText(this,"HelloException", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+    }
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ListView listView=findViewById(R.id.my_list);
-        List<String> mylist=new ArrayList<>();
-        mylist.add("Eraser");mylist.add("Pen");
-        mylist.add("Ink");mylist.add("Pencil");
-        mylist.add("Copy");mylist.add("Book");
+        load();
+        String temp=trie.retrieve();
+       // String temp1="abl";
+        List<String> mylist=trie.suffix(temp);
+        //Toast.makeText(MainActivity.this,temp.toString(),Toast.LENGTH_SHORT).show();
+       // mylist.add("Erase");
         arrayAdapter=new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,mylist);
         listView.setAdapter(arrayAdapter);
-
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.my_menu,menu);
