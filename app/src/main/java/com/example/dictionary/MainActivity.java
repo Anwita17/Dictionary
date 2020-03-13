f978bd2784882d3bd6881c58a9973f4903fa1fae
Rahul-Dey12 <r.dey711@gmail.com>
2020-03-14 01:34:26 +0530

    api intregation done

--- app/src/main/java/com/example/dictionary/MainActivity.java
+++ app/src/main/java/com/example/dictionary/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.dictionary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
+import android.widget.Toast;
 
 
 import java.util.ArrayList;
@@ -16,18 +20,34 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     ArrayAdapter<String> arrayAdapter;
 
+    private String dictionaryEntries(String Word) {
+        final String language = "en-gb";
+        final String word = Word;
+        final String fields = "definitions";
+        final String strictMatch = "false";
+        final String word_id = word.toLowerCase();
+        return "https://od-api.oxforddictionaries.com:443/api/v2/entries/" + language + "/" + word_id + "?" + "fields=" + fields + "&strictMatch=" + strictMatch;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ListView listView=findViewById(R.id.my_list);
-        List<String> mylist=new ArrayList<>();
+        final List<String> mylist=new ArrayList<>();
         mylist.add("Eraser");mylist.add("Pen");
         mylist.add("Ink");mylist.add("Pencil");
         mylist.add("Copy");mylist.add("Book");
         arrayAdapter=new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,mylist);
         listView.setAdapter(arrayAdapter);
-
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                new CallbackTask(MainActivity.this).execute(dictionaryEntries(mylist.get(i)));
+                //Toast.makeText(MainActivity.this,mylist.get(i),Toast.LENGTH_SHORT).show();
+            }
+        });
+        //Log.v("Result",dictionaryEntries());
     }
 
     @Override
@@ -52,3 +72,4 @@ public class MainActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 }
+
